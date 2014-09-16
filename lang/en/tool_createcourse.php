b3e841e361b05d04678befb03e6e606ddb293c28
nateyb93 <brownn15@up.edu>
2014-09-16 11:03:19 -0700

    added needed files

---
+++ lang/en/tool_createcourse.php
@@ -0,0 +1,38 @@
+<?php
+$string['pluginname'] = 'Create new course';
+$string['header'] = 'Create a new course to be entered into the course database';
+
+//database information
+$string['dbinfo_heading_name'] = 'dbinfo_heading';
+$string['dbinfo_heading_heading'] = 'Database information';
+$string['dbinfo_heading_info'] = 'Enter connection information for the course database';
+
+$string['dbinfo_protocol_name'] = 'dbinfo_protocol';
+$string['dbinfo_protocol_visiblename'] = 'Protocol';
+$string['dbinfo_protocol_description'] = 'Enter database protocol type here.';
+$string['dbinfo_protocol_default'] = '';
+
+$string['dbinfo_hostname_name'] = 'dbinfo_hostname';
+$string['dbinfo_hostname_visiblename'] = 'Hostname';
+$string['dbinfo_hostname_description'] = 'Enter database hostname address here.';
+$string['dbinfo_hostname_default'] = '';
+
+$string['dbinfo_portno_name'] = 'dbinfo_portno';
+$string['dbinfo_portno_visiblename'] = 'Port number';
+$string['dbinfo_portno_description'] = 'Enter database port number here.';
+$string['dbinfo_portno_default'] = '';
+
+//database login
+$string['dbauth_heading_name'] = 'dbauth_heading';
+$string['dbauth_heading_heading'] = 'Login information';
+$string['dbauth_heading_info'] = 'Enter database login information here';
+
+$string['dbauth_login_name'] = 'dbauth_login';
+$string['dbauth_login_visiblename'] = 'Username';
+$string['dbauth_login_description'] = 'Enter database username here.';
+$string['dbauth_login_default'] = '';
+
+$string['dbauth_password_name'] = 'dbauth_password';
+$string['dbauth_password_visiblename'] = 'Password';
+$string['dbauth_password_description'] = 'Enter database password here.';
+$string['dbauth_password_default'] = '';
---
+++ version.php
@@ -0,0 +1,4 @@
+<?php
+defined('MOODLE_INTERNAL') || die;
+$plugin->version = 2014091604;
+$plugin->requires = 2011120500;
